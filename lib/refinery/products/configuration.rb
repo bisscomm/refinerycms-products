767f676bd95560c6475cbb44bdaa0aa772a66aff
Brice Sanchez <brice@bisscomm.com>
2014-10-27 13:13:02 -0400

    Add products_per_page config

--- lib/refinery/products/configuration.rb
+++ lib/refinery/products/configuration.rb
@@ -2,8 +2,10 @@ module Refinery
   module Products
     include ActiveSupport::Configurable
 
+    config_accessor :products_per_page
     config_accessor :page_path_products_index, :page_path_products_categories_index
 
+    self.products_per_page = 10
     self.page_path_products_index = "/products"
     self.page_path_products_categories_index = "/products/categories"
   end
