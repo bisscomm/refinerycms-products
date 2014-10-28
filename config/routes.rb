4022824e4a87d6a8800811834cba9279c0358f83
Brice Sanchez <brice@bisscomm.com>
2014-10-28 16:36:33 -0400

    Improve routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Refinery::Core::Engine.routes.draw do
 
   # Frontend routes
-  namespace :products, :path => Refinery::Products.page_path_products_index do
+  namespace :products, :path => Refinery::Products.shop_path do
     root :to => "products#index"
-    resources :products, :only => [:show]
 
-    get 'categories/:id', :to => 'categories#show', :as => 'category'
+    scope :path => Refinery::Products.products_path do
+      resources :products, :path => '', :as => :products, :controller => 'products'
+    end
+
+    scope :path => Refinery::Products.products_categories_path do
+      resources :categories, :path => '', :as => :categories, :controller => 'categories'
+    end
   end
-
 
   # Admin routes
   namespace :products, :path => '' do
     namespace :admin, :path => Refinery::Core.backend_route do
-      scope :path => Refinery::Products.page_path_products_index do
+      scope :path => Refinery::Products.products_path do
         root :to => "products#index"
 
         resources :products, :except => :show do
