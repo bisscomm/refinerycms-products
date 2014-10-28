cbba52becdba64e91081fda6254a28d480d765dd
Brice Sanchez <brice@bisscomm.com>
2014-10-28 16:38:08 -0400

    Improve category model

--- app/models/refinery/products/category.rb
+++ app/models/refinery/products/category.rb
@@ -5,22 +5,24 @@ module Refinery
 
       translates :title, :slug
 
-      friendly_id :friendly_id_source, :use => [:slugged, :globalize]
+      friendly_id :title, :use => [:slugged, :globalize]
+
+      has_many :categorizations, :dependent => :destroy, :foreign_key => :products_category_id
+      has_many :products, :through => :categorizations, :source => :product
 
       validates :title, :presence => true, :uniqueness => true
 
       acts_as_indexed :fields => [:title]
 
-      # If title changes tell friendly_id to regenerate slug when
-      # saving record
-      def should_generate_new_friendly_id?
-        title_changed?
+      def self.translated
+        with_translations(::Globalize.locale)
       end
 
-      def friendly_id_source
-        title
+      def product_count
+        products.live.with_globalize.count
       end
 
+      self.per_page = Refinery::Products.products_per_page
     end
   end
 end
