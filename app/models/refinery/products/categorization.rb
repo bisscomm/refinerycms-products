1e209059ef632b7fe0bfb1cfcfe57b7e0aca5c0a
Brice Sanchez <brice@bisscomm.com>
2014-10-29 16:35:29 -0400

    Little clean space in categorization

--- app/models/refinery/products/categorization.rb
+++ app/models/refinery/products/categorization.rb
@@ -1,8 +1,8 @@
 module Refinery
   module Products
     class Categorization < ActiveRecord::Base
-
       self.table_name = 'refinery_products_categories_products'
+
       belongs_to :product,           :class_name => 'Refinery::Products::Product',  :foreign_key => :product_id
       belongs_to :products_category, :class_name => 'Refinery::Products::Category', :foreign_key => :products_category_id
     end
