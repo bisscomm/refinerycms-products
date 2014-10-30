423248798481753d2a5fea36adad427d72357b61
Brice Sanchez <brice@bisscomm.com>
2014-10-30 16:58:24 -0400

    Translate propertization model

--- app/models/refinery/products/propertization.rb
+++ app/models/refinery/products/propertization.rb
@@ -3,6 +3,8 @@ module Refinery
     class Propertization < ActiveRecord::Base
       self.table_name = 'refinery_products_properties_products'
 
+      translates :value
+
       belongs_to :product,           :class_name => 'Refinery::Products::Product',  :foreign_key => :product_id
       belongs_to :products_property, :class_name => 'Refinery::Products::Property', :foreign_key => :products_property_id
     end
--- db/migrate/5_create_properties_translations.rb
+++ db/migrate/5_create_properties_translations.rb
@@ -5,9 +5,16 @@ class CreatePropertiesTranslations < ActiveRecord::Migration
     }, {
       :migrate_data => true
     })
+
+    Refinery::Products::Propertization.create_translation_table!({
+      :value => :string
+    }, {
+      :migrate_data => true
+    })
   end
 
   def down
     Refinery::Products::Property.drop_translation_table! :migrate_data => true
+    Refinery::Products::Propertization.drop_translation_table! :migrate_data => true
   end
 end
