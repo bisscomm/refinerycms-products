932f8d8b6f98a50194106d17d4a0d422b9e2638f
Brice Sanchez <brice@bisscomm.com>
2014-10-30 09:31:15 -0400

    Fix seeds with new pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,57 @@
-Refinery::I18n.frontend_locales.each do |lang|
-  I18n.locale = lang
+::Refinery::User.all.each do |user|
+  user.plugins.where(:name => 'refinery-products').first_or_create!
+end if defined?(::Refinery::User)
 
-  Refinery::User.find_each do |user|
-    user.plugins.where(name: 'refinerycms-products').first_or_create!(
-      position: (user.plugins.maximum(:position) || -1) +1
-    )
-  end if defined?(Refinery::User)
+if defined?(::Refinery::Page)
+  shop_page = Refinery::Page.find_by(:link_url => Refinery::Products.shop_path)
 
-  Refinery::Page.where(link_url: (url = "/products")).first_or_create!(
-    title: 'Products',
-    deletable: false,
-    menu_match: "^#{url}(\/|\/.+?|)$"
-  ) do |page|
-    Refinery::Pages.default_parts.each_with_index do |part, index|
-      page.parts.build title: part, body: nil, position: index
-    end
-  end if defined?(Refinery::Page)
+  unless shop_page
+    shop_page = ::Refinery::Page.create({
+      :title => "Shop",
+      :link_url => Refinery::Products.shop_path,
+      :menu_match => "^(/products.*)|#{Refinery::Products.shop_path}$",
+      :deletable => false
+    })
+    shop_page.parts.create({
+      :title => "Body",
+      :body => "<p>This is the shop page.</p>",
+      :position => 0
+    })
+  end
+
+  products_url = "#{Refinery::Products.shop_path}#{Refinery::Products.products_path}"
+  unless Refinery::Page.where(:link_url => products_url).any?
+    products_page = shop_page.children.create({
+      :title => "Products",
+      :link_url => products_url,
+      :menu_match => "^(/shop/products)|#{products_url}$",
+      :show_in_menu => false,
+      :deletable => false
+    })
+    products_page.parts.create({
+      :title => "Body",
+      :body => "<p>This is the products page.</p>",
+      :position => 0
+    })
+  end
+
+  categories_url = "#{Refinery::Products.shop_path}#{Refinery::Products.products_categories_path}"
+  unless Refinery::Page.where(:link_url => categories_url).any?
+    categories_page = shop_page.children.create({
+      :title => "Categories",
+      :link_url => categories_url,
+      :menu_match => "^(/shop/categories)|#{categories_url}$",
+      :show_in_menu => false,
+      :deletable => false
+    })
+    categories_page.parts.create({
+      :title => "Body",
+      :body => "<p>This is the categories page.</p>",
+      :position => 0
+    })
+  end
 end
-Refinery::I18n.frontend_locales.each do |lang|
-  I18n.locale = lang
 
-  Refinery::User.find_each do |user|
-    user.plugins.where(name: 'refinerycms-products').first_or_create!(
-      position: (user.plugins.maximum(:position) || -1) +1
-    )
-  end if defined?(Refinery::User)
-
-  Refinery::Page.where(link_url: (url = "/products/categories")).first_or_create!(
-    title: 'Categories',
-    deletable: false,
-    menu_match: "^#{url}(\/|\/.+?|)$"
-  ) do |page|
-    Refinery::Pages.default_parts.each_with_index do |part, index|
-      page.parts.build title: part, body: nil, position: index
-    end
-  end if defined?(Refinery::Page)
+(Refinery::Inquiries::Setting.methods.sort - Refinery::Setting.methods).each do |setting|
+  Refinery::Inquiries::Setting.send(setting) if setting.to_s !~ /=\z/
 end
