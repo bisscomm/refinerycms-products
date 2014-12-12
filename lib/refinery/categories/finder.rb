fe84b20470c62e06eb94337c528328a9c0f8c7d6
Brice Sanchez <brice@bisscomm.com>
2014-12-12 17:00:18 -0500

    Add Products module in categories finder

--- lib/refinery/categories/finder.rb
+++ lib/refinery/categories/finder.rb
@@ -1,174 +1,176 @@
 module Refinery
-  module Categories
-    class Finder
-      def self.by_path(path)
-        FinderByPath.new(path).find
-      end
+  module Products
+    module Categories
+      class Finder
+        def self.by_path(path)
+          FinderByPath.new(path).find
+        end
 
-      def self.by_path_or_id(path, id)
-        FinderByPathOrId.new(path, id).find
-      end
+        def self.by_path_or_id(path, id)
+          FinderByPathOrId.new(path, id).find
+        end
 
-      def self.by_title(title)
-        FinderByTitle.new(title).find
-      end
+        def self.by_title(title)
+          FinderByTitle.new(title).find
+        end
 
-      def self.by_slug(slug, conditions = {})
-        FinderBySlug.new(slug, conditions).find
-      end
+        def self.by_slug(slug, conditions = {})
+          FinderBySlug.new(slug, conditions).find
+        end
 
-      def self.with_globalize(conditions = {})
-        Finder.new(conditions).find
-      end
+        def self.with_globalize(conditions = {})
+          Finder.new(conditions).find
+        end
 
-      def initialize(conditions)
-        @conditions = conditions
-      end
+        def initialize(conditions)
+          @conditions = conditions
+        end
 
-      def find
-        with_globalize
-      end
+        def find
+          with_globalize
+        end
 
-      def with_globalize
-        globalized_conditions = {:locale => ::Globalize.locale.to_s}.merge(conditions)
-        translations_conditions = translations_conditions(globalized_conditions)
+        def with_globalize
+          globalized_conditions = {:locale => ::Globalize.locale.to_s}.merge(conditions)
+          translations_conditions = translations_conditions(globalized_conditions)
 
-        # A join implies readonly which we don't really want.
-        Category.where(globalized_conditions).
-             joins(:translations).
-             where(translations_conditions).
-             readonly(false)
-      end
+          # A join implies readonly which we don't really want.
+          Category.where(globalized_conditions).
+               joins(:translations).
+               where(translations_conditions).
+               readonly(false)
+        end
 
-      private
-      attr_accessor :conditions
+        private
+        attr_accessor :conditions
 
-      def translated_attributes
-        Category.translated_attribute_names.map(&:to_s) | %w(locale)
-      end
+        def translated_attributes
+          Category.translated_attribute_names.map(&:to_s) | %w(locale)
+        end
 
-      def translations_conditions(original_conditions)
-        translations_conditions = {}
-        original_conditions.keys.each do |key|
-          if translated_attributes.include? key.to_s
-            translations_conditions["#{Category.translation_class.table_name}.#{key}"] = original_conditions.delete(key)
+        def translations_conditions(original_conditions)
+          translations_conditions = {}
+          original_conditions.keys.each do |key|
+            if translated_attributes.include? key.to_s
+              translations_conditions["#{Category.translation_class.table_name}.#{key}"] = original_conditions.delete(key)
+            end
           end
+          translations_conditions
         end
-        translations_conditions
+
       end
 
-    end
-
-    class FinderByTitle < Finder
-      def initialize(title)
-        @title = title
-        @conditions = default_conditions
-      end
-
-      def default_conditions
-        { :title => title }
-      end
-
-      private
-      attr_accessor :title
-    end
-
-    class FinderBySlug < Finder
-      def initialize(slug, conditions)
-        @slug = slug
-        @conditions = default_conditions.merge(conditions)
-      end
-
-      def default_conditions
-        {
-          :locale => Refinery::I18n.frontend_locales.map(&:to_s),
-          :slug => slug
-        }
-      end
-
-      private
-      attr_accessor :slug
-    end
-
-    class FinderByPath
-      def initialize(path)
-        @path = path
-      end
-
-      def find
-        if slugs_scoped_by_parent?
-          FinderByScopedPath.new(path).find
-        else
-          FinderByUnscopedPath.new(path).find
+      class FinderByTitle < Finder
+        def initialize(title)
+          @title = title
+          @conditions = default_conditions
         end
-      end
 
-      private
-      attr_accessor :path
-
-      def slugs_scoped_by_parent?
-        ::Refinery::Categories.scope_slug_by_parent
-      end
-
-      def by_slug(slug_path, conditions = {})
-        Finder.by_slug(slug_path, conditions)
-      end
-    end
-
-    class FinderByScopedPath < FinderByPath
-      def find
-        # With slugs scoped to the parent category we need to find a category by its full path.
-        # For example with about/example we would need to find 'about' and then its child
-        # called 'example' otherwise it may clash with another category called /example.
-        category = parent_category
-        while category && path_segments.any? do
-          category = next_category(category)
+        def default_conditions
+          { :title => title }
         end
-        category
+
+        private
+        attr_accessor :title
       end
 
-      private
+      class FinderBySlug < Finder
+        def initialize(slug, conditions)
+          @slug = slug
+          @conditions = default_conditions.merge(conditions)
+        end
 
-      def path_segments
-        @path_segments ||= path.split('/').select(&:present?)
+        def default_conditions
+          {
+            :locale => Refinery::I18n.frontend_locales.map(&:to_s),
+            :slug => slug
+          }
+        end
+
+        private
+        attr_accessor :slug
       end
 
-      def parent_category
-        by_slug(path_segments.shift, :parent_id => nil).first
-      end
+      class FinderByPath
+        def initialize(path)
+          @path = path
+        end
 
-      def next_category(category)
-        slug_or_id = path_segments.shift
-        category.children.by_slug(slug_or_id).first || category.children.find(slug_or_id)
-      end
-    end
-
-    class FinderByUnscopedPath < FinderByPath
-      def find
-        by_slug(path).first
-      end
-    end
-
-    class FinderByPathOrId
-      def initialize(path, id)
-        @path = path
-        @id = id
-      end
-
-      def find
-        if path.present?
-          if path.friendly_id?
-            Category.friendly.find_by_path(path)
+        def find
+          if slugs_scoped_by_parent?
+            FinderByScopedPath.new(path).find
           else
-            Category.friendly.find(path)
+            FinderByUnscopedPath.new(path).find
           end
-        elsif id.present?
-          Category.friendly.find(id)
+        end
+
+        private
+        attr_accessor :path
+
+        def slugs_scoped_by_parent?
+          ::Refinery::Categories.scope_slug_by_parent
+        end
+
+        def by_slug(slug_path, conditions = {})
+          Finder.by_slug(slug_path, conditions)
         end
       end
 
-      private
-      attr_accessor :id, :path
+      class FinderByScopedPath < FinderByPath
+        def find
+          # With slugs scoped to the parent category we need to find a category by its full path.
+          # For example with about/example we would need to find 'about' and then its child
+          # called 'example' otherwise it may clash with another category called /example.
+          category = parent_category
+          while category && path_segments.any? do
+            category = next_category(category)
+          end
+          category
+        end
+
+        private
+
+        def path_segments
+          @path_segments ||= path.split('/').select(&:present?)
+        end
+
+        def parent_category
+          by_slug(path_segments.shift, :parent_id => nil).first
+        end
+
+        def next_category(category)
+          slug_or_id = path_segments.shift
+          category.children.by_slug(slug_or_id).first || category.children.find(slug_or_id)
+        end
+      end
+
+      class FinderByUnscopedPath < FinderByPath
+        def find
+          by_slug(path).first
+        end
+      end
+
+      class FinderByPathOrId
+        def initialize(path, id)
+          @path = path
+          @id = id
+        end
+
+        def find
+          if path.present?
+            if path.friendly_id?
+              Category.friendly.find_by_path(path)
+            else
+              Category.friendly.find(path)
+            end
+          elsif id.present?
+            Category.friendly.find(id)
+          end
+        end
+
+        private
+        attr_accessor :id, :path
+      end
     end
   end
 end
