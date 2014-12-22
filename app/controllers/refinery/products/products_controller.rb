90a3d2787445bab2d3eb2d07abab405a8a7f2463
Brice Sanchez <brice@bisscomm.com>
2014-12-22 09:49:44 -0500

    Improve product views

--- app/controllers/refinery/products/products_controller.rb
+++ app/controllers/refinery/products/products_controller.rb
@@ -7,6 +7,12 @@ module Refinery
       before_filter :find_all_products
       before_filter :find_product, only: :show
 
+      def show
+        @product_images = @product.images
+        @root_category = @product.categories.root
+        @root_category_products = @root_category.products
+      end
+
       protected
 
         def find_page
