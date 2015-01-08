4c07e8e0f07e202fc943ba3b77493e1a7cda021f
Brice Sanchez <brice@bisscomm.com>
2015-01-08 13:50:45 -0500

    Improve seed to create pages in all frontend locales

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,52 +2,56 @@
   user.plugins.where(:name => 'refinery-products').first_or_create!
 end if defined?(::Refinery::User)
 
-if defined?(::Refinery::Page)
-  shop_page = Refinery::Page.find_by(:link_url => Refinery::Products.shop_path)
+Refinery::I18n.frontend_locales.each do |lang|
+  I18n.locale = lang
 
-  unless shop_page
-    shop_page = ::Refinery::Page.create({
-      :title => "Shop",
-      :link_url => Refinery::Products.shop_path,
-      :menu_match => "^(/products.*)|#{Refinery::Products.shop_path}$",
-      :deletable => false
-    })
-    shop_page.parts.create({
-      :title => "Body",
-      :body => "<p>This is the shop page.</p>",
-      :position => 0
-    })
-  end
+  if defined?(::Refinery::Page)
+    shop_page = Refinery::Page.find_by(:link_url => Refinery::Products.shop_path)
 
-  products_url = "#{Refinery::Products.shop_path}#{Refinery::Products.products_path}"
-  unless Refinery::Page.where(:link_url => products_url).any?
-    products_page = shop_page.children.create({
-      :title => "Products",
-      :link_url => products_url,
-      :menu_match => "^(/shop/products)|#{products_url}$",
-      :show_in_menu => false,
-      :deletable => false
-    })
-    products_page.parts.create({
-      :title => "Body",
-      :body => "<p>This is the products page.</p>",
-      :position => 0
-    })
-  end
+    unless shop_page
+      shop_page = ::Refinery::Page.create({
+        :title => "Shop",
+        :link_url => Refinery::Products.shop_path,
+        :menu_match => "^(/products.*)|#{Refinery::Products.shop_path}$",
+        :deletable => false
+      })
+      shop_page.parts.create({
+        :title => "Body",
+        :body => "<p>This is the shop page.</p>",
+        :position => 0
+      })
+    end
 
-  categories_url = "#{Refinery::Products.shop_path}#{Refinery::Products.products_categories_path}"
-  unless Refinery::Page.where(:link_url => categories_url).any?
-    categories_page = shop_page.children.create({
-      :title => "Categories",
-      :link_url => categories_url,
-      :menu_match => "^(/shop/categories)|#{categories_url}$",
-      :show_in_menu => false,
-      :deletable => false
-    })
-    categories_page.parts.create({
-      :title => "Body",
-      :body => "<p>This is the categories page.</p>",
-      :position => 0
-    })
+    products_url = "#{Refinery::Products.shop_path}#{Refinery::Products.products_path}"
+    unless Refinery::Page.where(:link_url => products_url).any?
+      products_page = shop_page.children.create({
+        :title => "Products",
+        :link_url => products_url,
+        :menu_match => "^(/shop/products)|#{products_url}$",
+        :show_in_menu => false,
+        :deletable => false
+      })
+      products_page.parts.create({
+        :title => "Body",
+        :body => "<p>This is the products page.</p>",
+        :position => 0
+      })
+    end
+
+    categories_url = "#{Refinery::Products.shop_path}#{Refinery::Products.products_categories_path}"
+    unless Refinery::Page.where(:link_url => categories_url).any?
+      categories_page = shop_page.children.create({
+        :title => "Categories",
+        :link_url => categories_url,
+        :menu_match => "^(/shop/categories)|#{categories_url}$",
+        :show_in_menu => false,
+        :deletable => false
+      })
+      categories_page.parts.create({
+        :title => "Body",
+        :body => "<p>This is the categories page.</p>",
+        :position => 0
+      })
+    end
   end
 end
